3748f92ea06fb671fc9f9f3f1223c976ebf4b123
mvstermind <zablockidawid1@gmail.com>
2024-04-16 20:46:29 +0200

    added sum cool remaps for go

--- init.lua
+++ init.lua
@@ -55,6 +55,20 @@ require("lazy").setup({
 	"brenoprata10/nvim-highlight-colors",
 	"ThePrimeagen/harpoon",
 	"jiangmiao/auto-pairs",
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	},
 
 	{
 		"nvim-telescope/telescope.nvim",
@@ -275,6 +289,7 @@ require("lazy").setup({
 
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
 		},
 		config = function()
 			local cmp = require("cmp")
@@ -444,14 +459,6 @@ vim.opt.termguicolors = true
 
 require("nvim-highlight-colors").setup({})
 
-vim.api.nvim_set_keymap(
-	"i",
-	"<leader>jj",
-	[[<Esc>:lua << EOF
-vim.api.nvim_put({ 'if err != nil {' }, 'l', true, true)
-vim.api.nvim_put({ '    return err' }, 'l', true, true)
-vim.api.nvim_put({ '}' }, 'l', true, true)
-vim.api.nvim_feedkeys('a', 'n', true)
-EOF<CR>]],
-	{ noremap = true }
-)
+vim.keymap.set("n", "<leader>e", ":GoIfErr<CR>")
+vim.keymap.set("n", "<leader>po", ":GoPkgOutline<CR>")
+vim.keymap.set("n", "<leader>t", ":GoAddAllTest")
