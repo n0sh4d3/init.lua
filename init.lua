f879d91e04d8afddec88362c9c18920179f6332c
mvstermind <zablockidawid1@gmail.com>
2024-05-08 19:06:24 +0200

    fixing

--- init.lua
+++ init.lua
@@ -11,24 +11,6 @@ require("lazy").setup({
 }, {})
 
 
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-h>", function()
-    ui.nav_file(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-    ui.nav_file(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-    ui.nav_file(3)
-end)
-vim.keymap.set("n", "<C-s>", function()
-    ui.nav_file(4)
-end)
-
 require("remaps.remaps")
 require("settings")
 require('gitsigns').setup()
